049647c6f777a3adea5e9577f3ace80bbfb3f4f2
Tommyz99 <62272220+Tommyz99@users.noreply.github.com>
2023-10-06 23:29:09 -0400

    Add files via upload

---
+++ public/Scripts/app.js
@@ -0,0 +1,12 @@
+// IIFE -- Immediately Invoked Function Expression
+// app.js | Tommy Zhou | 301257934 | October 6th, 2023
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+// users.js | Tommy Zhou | 301257934 | October 6th, 2023
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+module.exports = router;
